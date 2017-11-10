e3eb481ad2ba65a443daa3d27e369b94514e0962
R.Viana <ronanssviana@gmail.com>
2017-11-10 11:36:41 -0200

    Final commit
    Finish everything.

--- README.md
+++ README.md
@@ -1,18 +1,41 @@
 <h1>Trabalho da faculdade</h1>
+
 </br>
+
 <h2>Descrição</h2>
+
 <p>Este trabalho almeja pegar uma amostra de 1000 palavras e comparar com uma lista de radicais pre-estabelecidos</p>
 
-<h2>Como Rodar?</h2>
+<h2>Como Rodar? (Para quem realmente nao sabe nada deste mundo) </h2>
+
 <ul>
+
   <li>É necessario ter o Node instalado em sua maquina => <a href="https://nodejs.org/en/" target="_blank">NodeJS</a></li>
-  <li>Rodar os seguintes comandos</li>
+
+  <li>Dica para Windows: crie uma pasta chamada "nodejs"  diretamente no  "C:\\" faça a instalação do node dentro desta pasta. Para ver se a instalação ocorreu perfeitamente abra o CMD e digite "node -v" esse comando retornará a versão instalada do node em sua máquina.</li>
+
+  <li>Dica para linux : Siga esse passo a passo  da página <a href="https://tableless.com.br/como-instalar-node-js-no-linux-corretamente-ubuntu-debian-elementary-os/" target="_blank">Tabless</a> para uma instalação perfeita do Node em seu sistema linux. Lembrando de  entrar no repositório do nvm para verificar a versão atual <a href="https://github.com/creationix/nvm" target="_blank">(NVM)</a></li>
+
+  <li>Dica para Mac : Siga este passo a passo da página <a href="http://blog.teamtreehouse.com/install-node-js-npm-mac" target="_blank">Treehouse</a> ele mostra  a instalação do Homebrew que nada mais é que um gerenciador de pacotes para Mac e por fim mostra  a instalação do Node.</li>
+
+</ul>
+
+<ul>
+  <li>Abra o terminal e vá até a pasta do projeto. Com alguns comandos cd você chega lá.<a href="https://www.linux.ime.usp.br/~lucasmmg/livecd/documentacao/documentos/terminal/Terminal_basico.html" target="_blank"> (Comandos para terminais)</a>
+  </li>
+
+  <li>Dentro da pasta, rodar os seguintes comandos</li>
   
   ```
-  
+  npm install -g json-server
   npm install 
   npm run start-server
   
   ```
+  
+  <li>Minimize o terminal</li>
+
   <li>Abrir seu navegador na pagina index.html do projeto</li>
+
+  <li>Agora é so usufluir da aplicação, mas lembre-se, com moderação \(*__*)/.</li>
 </ul>
--- sketch.js
+++ sketch.js
@@ -11,13 +11,13 @@ $(function() {
     var showGenRadicals = $('#show-genradical');
 
     
-    $.getJSON('http://localhost:3000/db', function (data) {
+    $.getJSON('http://localhost:3000/db', function (data) {// PEGA TODOS OS DADOS DO SERVIDOR LOCAL DISPONIBILIZA
     
         var words = data.words;
         var radicals = data.radicals;
         var suffixes = data.sufixos;
 
-        $('#get_words').click(function () {
+        $('#get_words').click(function () {// MOSTRA A LISTA DE 1000 PALAVRAS
             if(words.length){
                 var content = '<li>'+ words.join('</li><li>') +'</li>';
                 var list = $('<ul />').html(content);
@@ -27,7 +27,7 @@ $(function() {
             showWords.toggle();
         });
 
-        $('#get_radicals').click(function () {
+        $('#get_radicals').click(function () {// MOSTRA A LISTA DE  RADICALS TRATATOS SEM REPETIÇÃO
             if(radicals.length){
                 var content = '<li>'+ radicals.join('</li><li>') +'</li>';
                 var list = $('<ul />').html(content);
@@ -43,22 +43,20 @@ $(function() {
 
             var hits = 0;
             var listNewRadicals = [];
+            var haveAlreadyCount = 0;
  
             for (var w = 0; w < words.length ; w++) {
-
-                var haveAlreadyCount = 0;
-                var wordSize =  words[w].length;
+                haveAlreadyCount = 0;
+                var wordSize =  words[w].length;// ARMAZENA O TAMANHO DA PALAVRA
 
                 for (var s = 0; s < suffixes.length; s++) {
-                    var sufixSize = suffixes[s].length;
+                    var sufixSize = suffixes[s].length;// ARMAZENA O TAMANHO DO SUFIXO
                     
                     if(words[w].endsWith(suffixes[s]) ==  true){
                         while(haveAlreadyCount < 1){
-                            var cutWord = (wordSize - sufixSize) - 1;
+                            var cutWord = (wordSize - sufixSize) - 1;// PONTO DE CORTE PARA A RETIRADA DO SUFIXO
                                                         
-                            listNewRadicals[w] = words[w].slice(0, cutWord);
-
-                            console.log(listNewRadicals[w]);
+                            listNewRadicals[w] = words[w].slice(0, cutWord);// RETIRO O SUFIXO DA PALAVRA A PARTIR DO PONTO DE CORTE
 
                             haveAlreadyCount = 1;
                         }
@@ -68,13 +66,29 @@ $(function() {
                 }                 
             }
 
-            var fnlPercent = Math.floor(( hits * 100 )/ 1000);
+            for (var m = 0; m < listNewRadicals.length; m++) {
+                haveAlreadyCount = 0;
+                for(var n = 0; n < radicals.length; n++) {
+                    if (listNewRadicals[m] == radicals[n]) {
+                        //console.log("===>"+ listNewRadicals[m] +"  equals  "+ radicals[n] +"<==="); IF YOU WANNA SEE  TE COMPARISON
+                        hits = hits + 1;
+                    }
+                }
+            }
+            var fnlPercent = Math.floor(( hits * 100 )/ 100);// CRIA A PORCENTAGEM A PARTIR DA COMPARAÇÃO DO MATCH ENTRE LISTA OFERECIDA COM A GERADA
             showPercentage.html("<h1>"+ fnlPercent + "% acertos</h1>");
 
             $('#show-percentage').toggle();
         });
-
-
     
-      });
+    });
 });
+
+//the rockafeller skank - fatboy Slim, 
+//we used to be friends -dandy warhols, 
+//l.s.f - kasabian, 
+//jerk it out - caesars
+//young folks - peter bjorn
+// mercy - duffy
+//the big big bang - rock mafia
+//club foot- kasabian
